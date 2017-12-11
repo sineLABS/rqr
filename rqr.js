ec05f42cf02e5cc05722a74d6023d31f77ca80f2
Joe Bryan <joe@sinelabs.com>
2017-12-11 15:25:10 -0500

    Syntax corrections

--- rqr.js
+++ rqr.js
@@ -17,15 +17,15 @@ rqr.root = makeRoot();
 rqr.withRoot = function(root) {
   var _rqr = function(module) {
     return require(path.join(_rqr.root, module));
-  }
+  };
   _rqr.root = root;
   return _rqr;
 };
 
 rqr.withPrefix = function(prefix) {
   var _rqr = function(module) {
-    return require(path.join(_rqr.root, module))
-  }
+    return require(path.join(_rqr.root, module));
+  };
   _rqr.root = makeRoot(prefix);
   return _rqr;
 };
